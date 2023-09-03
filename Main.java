f6355fa63139b1db8bc627110df5fc5822dc0ae9
Olegk2004 <52634890+Olegk2004@users.noreply.github.com>
2023-09-03 16:46:08 +0300

    Update 1я практика 4 задание

--- Main.java
+++ Main.java
@@ -1,30 +1,35 @@
 import java.util.Arrays;
 import java.util.OptionalInt;
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Введите размер массива (одно целочисленное значение): ");
+        System.out.println("Введите размер массива (одно целочисленное значение в диапазоне[-999999999;999999999]): ");
         int size = in.nextInt();
+        while (size <= 0) {
+            System.out.println("Длина массива не может быть <= 0. Попробуйте снова!");
+            size = in.nextInt();
+        }
         int i = 0;
         int[] arr = new int[size];
         int summa = 0;
-        int min = 999999999;
-        int max = -999999999;
-        while (i != size){
-            System.out.println("Введите следующее значение(число): ");
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        while (i != size) {
+            System.out.println("Введите следующее значение(целое число в диапазоне[-999999999;999999999]): ");
             arr[i] = in.nextInt();
             summa += arr[i];
-            if(arr[i] < min){
+            if (arr[i] < min) {
                 min = arr[i];
             }
-            if(arr[i] > max){
+            if (arr[i] > max) {
                 max = arr[i];
             }
-            i ++;
+            i++;
         }
-        System.out.println("Минимальный элемент в массиве: "+min);
-        System.out.println("Максимальный элемент в массиве: "+max);
-        System.out.println("Сумма элементов в массиве: "+summa);
+        System.out.println("Минимальный элемент в массиве: " + min);
+        System.out.println("Максимальный элемент в массиве: " + max);
+        System.out.println("Сумма элементов в массиве: " + summa);
     }
 }

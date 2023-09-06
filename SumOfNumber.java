f7723a09547c60931336a4ce766cfea19e280349
Olegk2004 <52634890+Olegk2004@users.noreply.github.com>
2023-09-07 00:32:30 +0300

    8 практика, 5 задание

---
+++ SumOfNumber.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите n: ");
+        int n = scanner.nextInt();
+        int summ = 0;
+        System.out.println(recursion(n, summ));
+    }
+
+    public static int recursion(int n, int summ) {
+        if (n == 0) {
+            return summ;
+        }
+        summ += n%10;
+        return recursion(n/10, summ);
+    }
+}

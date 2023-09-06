ac2a7b6f6cef08b300e9b735f513d61dec642da6
Olegk2004 <52634890+Olegk2004@users.noreply.github.com>
2023-09-07 00:50:22 +0300

    8 практика 8 задание

---
+++ Palindrome.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите слово: ");
+        String n = scanner.nextLine();
+        String summ = "";
+        int k = n.length()-1;
+        summ = recursion(n, k, summ);
+        if(n.equals(summ)){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
+    }
+
+    public static String recursion(String n, int k, String summ) {
+        if (k == -1) {
+            return summ;
+        }
+        summ += n.charAt(k);
+        return recursion(n, k-1, summ);
+    }
+}

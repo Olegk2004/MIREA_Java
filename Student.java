c8b20292bab210d053d94ce741ee56d1b9b0e2cd
Olegk2004 <52634890+Olegk2004@users.noreply.github.com>
2023-09-07 01:25:03 +0300

    9 практика 1 задача класс студента

---
+++ Student.java
@@ -0,0 +1,21 @@
+public class Student implements Comparable<Student>{
+    private int id;
+    private String name;
+
+    public Student(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+    public int getId(){
+        return this.id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int compareTo(Student otherStudent) {
+        // Сравниваем объекты Student на основе iDNumber
+        return Integer.compare(this.getId(), otherStudent.getId());
+    }
+}

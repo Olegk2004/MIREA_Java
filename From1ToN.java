d4462797aa00de24fd250954bdb4dad42e6f8e1e
Olegk2004 <52634890+Olegk2004@users.noreply.github.com>
2023-09-07 14:52:06 +0300

    Практика 9 задание 2

--- From1ToN.java
+++ From1ToN.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+class From1ToN {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите n от 2 до 3000: ");

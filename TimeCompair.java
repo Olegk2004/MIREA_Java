42dd9b0a2f843d41ddf0ee93692674547d9e0eec
Olegk2004 <52634890+Olegk2004@users.noreply.github.com>
2023-09-07 14:56:05 +0300

    Практика 11 задание 2

--- TimeCompair.java
+++ TimeCompair.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.Date;
 
-public class Main {
+public class TimeCompair {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Введите время в милисекундах:");

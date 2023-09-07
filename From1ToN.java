4df67a8478a153c7702d74ccaecb21a6136ad59c
Olegk2004 <52634890+Olegk2004@users.noreply.github.com>
2023-09-07 12:41:25 +0300

    8 практика 2 задание

--- From1ToN.java
+++ From1ToN.java
@@ -3,8 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите n: ");
+        System.out.print("Введите n от 2 до 3000: ");
         int n = scanner.nextInt();
+        while(n>3000 || n < 2){ 
+            System.out.print("Введите нормальный n: ");
+            n = scanner.nextInt();
+        }
         int x = n;
         recursion(n, x);
     }

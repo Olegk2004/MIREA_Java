e4e7a26872fa01ee6cf17f879ece4409f86957da
Olegk2004 <52634890+Olegk2004@users.noreply.github.com>
2023-09-08 09:46:12 +0300

    14 практика 1, 2 и 5 задания

--- Regular.java
+++ Regular.java
@@ -20,4 +20,30 @@ public class regular {
         Matcher m = regular.matcher(input);
         return m.matches();
     }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите № задания(1, 2, 5): ");
+        int num = in.nextInt();
+        regular reg = new regular();
+        switch (num){
+            case(1):
+                System.out.println("Введите строку: ");
+                String s = in.nextLine();
+                System.out.println("Введите разделитель в виде регулярного выражения: ");
+                String r = in.nextLine();
+                reg.Ex1(s, r);
+                break;
+            case(2):
+                System.out.println("Введите строку: ");
+                String s1 = in.nextLine();
+                reg.Ex2(s1);
+                break;
+            case(5):
+                System.out.println("Введите строку: ");
+                String s2 = in.nextLine();
+                reg.Ex5(s2);
+                break;
+        }
+    }
 }
